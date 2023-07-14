f44cb5269d038f5b1f12f59204fe26245ed8511b
St0new4ll <nstone@unomaha.edu>
2023-07-13 22:26:51 -0500

    Update Request.js

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,39 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { EventContext } from './EventContext';
 
-const ManagerDashboard = ({onDeleteEvent, onAddEvent}) => {
-  const {events, addEvent, deleteEvent} = useContext(EventContext);
-  const [newEvent, setNewEvent] = useState('');
-  const [time, setTime] = useState('');
-  const [city, setCity] = useState('');
-  
-
-  const handleAddEvent = () => {
-    addEvent(`${newEvent} - ${time} - ${city}`)
-    setNewEvent('');
-    setTime('');
-    setCity('');
-  };
-
-
-  const handleDeleteEvent = (index) => {
-    deleteEvent(index);
-  };
-
-  return (
-    <div>
-      <h2>Manager Dashboard</h2>
-      <h3>Events:</h3>
-      <ul class="Events">
-        {events.map((event, index) => (
-          <li key={index}>
-            {event} 
-            
-            <button onClick={() => handleDeleteEvent(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <div>
+<div>
         <h3>Add Event:</h3>
         <input 
           required
@@ -42,24 +10,6 @@ const ManagerDashboard = ({onDeleteEvent, onAddEvent}) => {
           Placeholder="Event Name"
           onChange={(e) => setNewEvent(e.target.value)}
         />
-        <input 
-          required
-          type="text"
-          value={time}
-          placeholder="Time"
-          onChange={(e) => setTime(e.target.value)}
-        />
-        <input
-          required
-          type="text"
-          value={city}
-          placeholder="City"
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button onClick={handleAddEvent}>Add Event</button>
-      </div>
-    </div>
-  );
-};
+</div>
 
-export default ManagerDashboard;
+export default Request;
